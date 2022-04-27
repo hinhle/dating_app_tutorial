008d5909391a2c6f9d285c07b7ac607edcb92a23
hinh.le <levhinhvh@gmail.com>
2022-04-27 14:07:15 +0700

    End of Section 6 (Removed unsued code)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ToastrModule } from 'ngx-toastr';
 
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
